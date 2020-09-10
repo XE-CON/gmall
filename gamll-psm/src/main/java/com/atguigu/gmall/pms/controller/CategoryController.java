1dc83adeca23090927de7f5dcc2e4849fc008052
Administrator <ss>
2020-09-10 13:04:13 +0800

    2020-9-10提交

--- gamll-psm/src/main/java/com/atguigu/gmall/pms/controller/CategoryController.java
+++ gamll-psm/src/main/java/com/atguigu/gmall/pms/controller/CategoryController.java
@@ -1,11 +1,13 @@
 package com.atguigu.gmall.pms.controller;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 import com.atguigu.core.bean.PageVo;
 import com.atguigu.core.bean.QueryCondition;
 import com.atguigu.core.bean.Resp;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gmall.pms.entity.CategoryEntity;
 import com.atguigu.gmall.pms.service.CategoryService;
-
-
 
 
 /**
@@ -32,12 +32,29 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @GetMapping
+    public Resp<List<CategoryEntity>> queryCategoriesByPidOrLevel(@RequestParam(value = "level", defaultValue = "0") Integer level,
+                                                                  @RequestParam(value = "parentCid",required = false) Long pid) {
+        QueryWrapper<CategoryEntity> wrapper = new QueryWrapper<>();
+        //判断分类的级别是否为0
+        if(level!=0){
+            wrapper.eq("",level);
+        }
+        //判断父节点的id是否为空
+        if (pid!=null){
+            wrapper.eq("parent_cid",pid);
+        }
+        List<CategoryEntity> categoryEntities=categoryService.list(wrapper);
+        return Resp.ok(categoryEntities);
+    }
+
     /**
      * 列表
      */
     @ApiOperation("分页查询(排序)")
     @GetMapping("/list")
     @PreAuthorize("hasAuthority('pms:category:list')")
+
     public Resp<PageVo> list(QueryCondition queryCondition) {
         PageVo page = categoryService.queryPage(queryCondition);
 
@@ -51,8 +68,8 @@ public class CategoryController {
     @ApiOperation("详情查询")
     @GetMapping("/info/{catId}")
     @PreAuthorize("hasAuthority('pms:category:info')")
-    public Resp<CategoryEntity> info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public Resp<CategoryEntity> info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
 
         return Resp.ok(category);
     }
@@ -63,8 +80,8 @@ public class CategoryController {
     @ApiOperation("保存")
     @PostMapping("/save")
     @PreAuthorize("hasAuthority('pms:category:save')")
-    public Resp<Object> save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public Resp<Object> save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
 
         return Resp.ok(null);
     }
@@ -75,8 +92,8 @@ public class CategoryController {
     @ApiOperation("修改")
     @PostMapping("/update")
     @PreAuthorize("hasAuthority('pms:category:update')")
-    public Resp<Object> update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public Resp<Object> update(@RequestBody CategoryEntity category) {
+        categoryService.updateById(category);
 
         return Resp.ok(null);
     }
@@ -87,8 +104,8 @@ public class CategoryController {
     @ApiOperation("删除")
     @PostMapping("/delete")
     @PreAuthorize("hasAuthority('pms:category:delete')")
-    public Resp<Object> delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+    public Resp<Object> delete(@RequestBody Long[] catIds) {
+        categoryService.removeByIds(Arrays.asList(catIds));
 
         return Resp.ok(null);
     }
